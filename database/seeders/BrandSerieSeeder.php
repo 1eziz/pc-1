b91ebabdde04a36f88799e649e58ef2ec9329caf
Sapar <sprmrt.glm@gmail.com>
2023-09-29 21:30:17 +0500

    1.1.0

---
+++ database/seeders/BrandSerieSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BrandSerieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
